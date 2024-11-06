cb2f34bec851f64ef64e5edf0b33854e2b529f52
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-11-06 22:40:47 +0700

    Menambahkan fitur ViewBinding, memperbarui compileSdk ke 35, dan memperbarui versi Kotlin

--- app/src/main/java/com/capstone/pawcheck/MainActivity.kt
+++ app/src/main/java/com/capstone/pawcheck/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.capstone.pawcheck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
