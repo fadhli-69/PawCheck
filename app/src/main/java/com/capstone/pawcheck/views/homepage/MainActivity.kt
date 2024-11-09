344bec150a28aba1225caa5953754ee2d2adec15
Dimaz Anugrahman Yusuf <dimazangrah64@gmail.com>
2024-11-09 17:31:20 +0800

    Update : - Change AGP to 8.6.0 - Installing KSP to Project - Add Retrofit, Room, Glide, ViewPager2.

--- app/src/main/java/com/capstone/pawcheck/views/homepage/MainActivity.kt
+++ app/src/main/java/com/capstone/pawcheck/views/homepage/MainActivity.kt
@@ -1,10 +1,7 @@
-package com.capstone.pawcheck
+package com.capstone.pawcheck.views.homepage
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.capstone.pawcheck.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
