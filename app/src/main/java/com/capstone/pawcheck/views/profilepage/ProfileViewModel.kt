63b70b6c67ed0d5577c2a4c42dc8e9a7564993ca
Muhammad Nurfadhli <79645149+mozart-69@users.noreply.github.com>
2024-11-28 06:41:39 +0700

    Menambahkan ProfileViewModel sebagai ViewModel untuk halaman profil (masih kosong)

---
+++ app/src/main/java/com/capstone/pawcheck/views/profilepage/ProfileViewModel.kt
@@ -0,0 +1,10 @@
+package com.capstone.pawcheck.views.profilepage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor() : ViewModel() {
+    // sementara
+}
