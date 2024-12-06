f64f6c33ac3bce853f2ca726a3187ab4d13424fc
Dimaz Anugrahman Yusuf <dimazangrah64@gmail.com>
2024-12-07 00:52:14 +0800

    Model Helper Sementara

--- app/src/main/java/com/capstone/pawcheck/views/result/ScanResultActivity.kt
+++ app/src/main/java/com/capstone/pawcheck/views/result/ScanResultActivity.kt
@@ -1,9 +1,14 @@
 package com.capstone.pawcheck.views.result
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.capstone.pawcheck.ModelHelper
 import com.capstone.pawcheck.databinding.ActivityScanResultBinding
+import androidx.camera.core.ImageProxy
 
 class ScanResultActivity : AppCompatActivity() {
 
@@ -15,9 +20,16 @@ class ScanResultActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val imageUriString = intent.getStringExtra("image_uri")
+        val disease = intent.getStringExtra("disease")
+        val diagnosis = intent.getStringExtra("diagnosis")
+
         if (imageUriString != null) {
             val imageUri = Uri.parse(imageUriString)
             binding.tvScanResult.setImageURI(imageUri)
         }
+
+        binding.diagnoses.text = disease ?: "Unknown"
+        binding.tvDiagnosesDescription.text = diagnosis ?: "No diagnosis available."
     }
 }
+
