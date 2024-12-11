9c9657e3de554dfdb4b0ce8945973c1bde78cf02
Dimaz Anugrahman Yusuf <dimazangrah64@gmail.com>
2024-12-11 15:26:49 +0800

    API Configuration

--- app/src/main/java/com/capstone/pawcheck/data/remote/connect/ApiService.kt
+++ app/src/main/java/com/capstone/pawcheck/data/remote/connect/ApiService.kt
@@ -1,4 +1,9 @@
 package com.capstone.pawcheck.data.remote.connect
 
+import com.capstone.pawcheck.data.remote.response.ResponseArticle
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("api/articles")
+    suspend fun getArticle() : ResponseArticle
 }
--- app/src/main/java/com/capstone/pawcheck/data/remote/response/ArticleResponse.kt
+++ app/src/main/java/com/capstone/pawcheck/data/remote/response/ArticleResponse.kt
@@ -1,10 +1,10 @@
-package com.capstone.pawcheck.data.remote.response
-
-data class ArticleResponse(
-     val id : String,
-     val title : String,
-     val image : String,
-     val content : String,
-     val author : String,
-     val createdAt: String,
-)
+//package com.capstone.pawcheck.data.remote.response
+//
+//data class ArticleResponse(
+//     val id : String,
+//     val title : String,
+//     val image : String,
+//     val content : String,
+//     val author : String,
+//     val createdAt: String,
+//)
